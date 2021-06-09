be749d68615287522dc867a85dc0a580feda783d
rom1313 <76427184+rom1313@users.noreply.github.com>
2021-06-09 14:58:32 +0200

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,12 @@
+message = "Hello"
+print(message)
+message = 1+ 2+3+4
+print(message)
+if message ==10:
+                print('ok')
+                message+=message+1
+elif message ==9:
+                print('nope')
+
+
+print(message)
