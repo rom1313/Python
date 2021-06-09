e710d87f6135432d97144a3209157b4897d8a7c6
rom1313 <76427184+rom1313@users.noreply.github.com>
2021-06-09 15:37:49 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -17,3 +17,16 @@ helloworld = "hello" + " " + "world"  # Concaténation String basique
 print(helloworld) 
 helloworld = "hello" * 10             # String + * = hellohellohello... X10 
 print(helloworld)
+tableau = []
+tableau2 = [1,2,3]                          # Tableau
+tableau.append(helloworld)            # Tableau append
+tableau.append(13)
+tableau.append(1153)
+print(tableau[0]) # prints hellohellohello....
+print(tableau[1]) # prints 13
+
+
+for element in tableau:               # print tous les éléments du tableau, element = variable au choix
+    print(element)
+for element in tableau2:               # print tous les éléments du tableau2, element = variable au choix
+    print(element)
