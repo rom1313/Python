61f980207c233ecf6ff3cc75087ad5b8a28bde42
rom1313 <76427184+rom1313@users.noreply.github.com>
2021-06-09 15:02:35 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,11 +1,12 @@
+#Les variables n'ont pas besoin d'être typé 
 message = "Hello"
 print(message)
-message = 1+ 2+3+4
+message = 1+2+3+4
 print(message)
 if message ==10:
-                print('ok')
+                print('ok')           # 4 indentations pour remplacer les {}
                 message+=message+1
-elif message ==9:
+elif message ==9:                     #elif = else if                    
                 print('nope')
 
 
